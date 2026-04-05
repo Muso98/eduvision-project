28d174215a23aa901d4ff4aab6740101a0dc1228
Muso98 <musabekmusayev98@gmail.com>
2026-04-05 19:44:05 +0200

    Relocate database to persistent data directory to fix operational errors

--- backend/eduvision/settings.py
+++ backend/eduvision/settings.py
@@ -73,7 +73,7 @@ if config('DB_HOST', default='localhost') == 'localhost':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': BASE_DIR / 'data' / 'db.sqlite3',
         }
     }
 else:
